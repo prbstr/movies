dff479a6b2e166750f45394402986d788259736b
parbster <parbster@gmail.com>
2022-03-15 22:04:17 +0200

    Add base project structure

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import homeReducer from '../features/home/home-reducer';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    home: homeReducer,
   },
 });
---
+++ src/features/home/home-reducer.jsx
@@ -0,0 +1,7 @@
+const homeReducer = (state = {}, action) => {
+  switch (action.payload) {
+    default:
+      return state;
+  }
+}
+export default homeReducer;
