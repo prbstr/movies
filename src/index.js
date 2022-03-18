fa2682a1f2c5995c1883bb0f2bd1187c2d7de2bc
parbster <parbster@gmail.com>
2022-03-18 17:02:08 +0200

    Update filename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/App";
-import { store } from "./app/Store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/Theme";
